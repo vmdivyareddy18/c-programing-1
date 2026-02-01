9538aad17263edf1fad823110461449164f07908
vmdivyareddy18 <vmdivyareddy18@gmail.com>
2026-02-01 21:26:10 +0530

    helo

--- new1.c
+++ new1.c
@@ -3,6 +3,6 @@ int main() {
     int number;
     printf("Enter a number: ");
     scanf("%d", &number);
-    printf("Cube of number is : %d\n",number * number);
+    printf("Cube of number is : %d\n",number * number * number);
     return 0;
 }
